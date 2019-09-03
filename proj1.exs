aa8656ef4b9b4a53ed68a8a43c7d2e1040336f71
Vaibhav <vsahay28@gmail.com>
2019-09-03 02:35:55 -0400

    added Supervisor.start_link

--- proj1.exs
+++ proj1.exs
@@ -1,4 +1,3 @@
-#Reading the command line arguments and passing them to the worker
-[arg1,arg2] = Enum.map(System.argv,fn x -> String.to_integer(x) end)
-Proj1.start()
-
+# Reading the command line arguments and passing them to the worker
+[low, high] = Enum.map(System.argv(), fn x -> String.to_integer(x) end)
+ProjSupervisor.start_link([low, high])

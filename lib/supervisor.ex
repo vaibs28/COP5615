c387b47abd8291b8ee529122a17f7ff54c8e7ce5
Vaibhav <vsahay28@gmail.com>
2019-09-04 15:35:48 -0400

    added spawn_link to spawn multiple workers

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,12 +1,27 @@
 defmodule ProjSupervisor do
   use Supervisor
 
-  def start_link() do
-    Supervisor.start_link(__MODULE__, :ok)
+  def start_link([low,high]) do
+    IO.puts("#{low}")
+    Supervisor.start_link(__MODULE__, [low,high])
   end
 
-  def init(param) do
-    children = [worker(Worker, [])]
-    Supervisor.init(children, strategy: :one_for_one)
+  # runs implicitly when calling start_link()
+  def init([low, high]) do
+    # calling Worker module
+    IO.puts("in supervisor init")
+    chunkSize = div(high - low, 4)
+    range = 0..3
+
+    # children = [worker(Worker, [])]
+
+    Enum.map(range, fn s ->
+      spawn_link(Worker, :init, [low + s * chunkSize, low + (s + 1) * chunkSize, s])
+    end)
+
+    :timer.sleep(5_000)
+
+    # supervise(children, strategy: :one_for_one)
+
   end
 end
